0ae82d65e996239bb85c29800325637ea30888af
Pedro Neto <pedro.santosnt@hotmail.com>
2020-11-20 09:40:55 -0300

    Finished challenge 1-3

--- Challenge(project1-3)/Challenge(project1-3)/ViewController.swift
+++ Challenge(project1-3)/Challenge(project1-3)/ViewController.swift
@@ -7,13 +7,50 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UITableViewController {
+    
+    var flags = [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        title = "Fun with Flags"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        
+        let fm = FileManager.default
+        let path = Bundle.main.resourcePath!
+        let items = try! fm.contentsOfDirectory(atPath: path)
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .action, target: self, action: nil)
+        
+        for item in items {
+            if item.hasSuffix(".png") {
+                flags.append(item)
+            }
+        }
     }
-
-
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return flags.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let flag = flags[indexPath.row]
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Flag", for: indexPath)
+        
+        cell.imageView!.image = UIImage(named: flag)
+        cell.textLabel?.text = "\(flag[..<flag.index(flag.endIndex, offsetBy: -4)].uppercased())" // flag name without the file extension
+        
+        return cell
+        
+    }
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if let vc = storyboard?.instantiateViewController(identifier: "Detail") as? DetailViewController {
+            vc.selectedImage = flags[indexPath.row]
+            navigationController?.pushViewController(vc, animated: true)
+        }
+    }
+    
 }
 
