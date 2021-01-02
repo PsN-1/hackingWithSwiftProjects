4cbe1cbf796faa50632962844f1faad43a8fa4d2
Pedro Neto <pedro.santosnt@hotmail.com>
2021-01-02 14:22:10 -0300

    Finish challenge project 13-15, fixed web view for svg images, add the filter func, and minor ui improv.

--- Challenge(project13-15)/Challenge(project13-15)/DetailViewController.swift
+++ Challenge(project13-15)/Challenge(project13-15)/DetailViewController.swift
@@ -16,27 +16,60 @@ class DetailViewController: UIViewController {
     @IBOutlet var capitalTextField: UITextField!
     @IBOutlet var subregionTextField: UITextField!
     @IBOutlet var populationTextField: UITextField!
-    @IBOutlet var currenciesTextField: UITextField!
-    @IBOutlet var langaguesTextField: UITextField!
+    
+    @IBOutlet var currencyLabel: UILabel!
+    @IBOutlet var languagesLabel: UILabel!
     
     var selectedCountry: Country?
-    var cells = [String]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        print(selectedCountry!)
         title = selectedCountry?.name
         
-        let fileURL:URL = URL(string: selectedCountry!.flag)!
-        let req = URLRequest(url: fileURL)
-        webView.load(req)
-        print(selectedCountry!)
+        let html = """
+        <html>
+            <head>
+                <meta name="viewport" content="width=device-width, initial-scale=1">
+                <style> body, html {
+                    height: 100%;
+                  }
+                  body {
+                    background-image: url("\(selectedCountry!.flag)");
+                    height: 100%;
+                    background-position: center;
+                    background-repeat: no-repeat;
+                    background-size: cover;
+                  }
+                </style>
+            </head>
+            <body>
+            </body>
+        </html>
+        """
+        
+        webView.loadHTMLString(html, baseURL: nil)
         
         nameTextField.text = selectedCountry?.name
         capitalTextField.text = selectedCountry?.capital
         subregionTextField.text = selectedCountry?.subregion
-        populationTextField.text = String(selectedCountry!.population)
+        
+        let numberFormatter = NumberFormatter()
+        numberFormatter.numberStyle = .decimal
+        let formattedPopulation = numberFormatter.string(from: NSNumber(value: selectedCountry!.population))
+        populationTextField.text = formattedPopulation
+        
+        for currency in 0..<(selectedCountry?.currencies!.count)! {
+            let coin = selectedCountry!.currencies![currency]
+            if let cSymbol = coin.symbol, let cCode = coin.code, let cName = coin.name {
+                currencyLabel.text! += "\(cSymbol) - \(cCode) (\(cName)). "
+            }
+        }
+        
+        for language in 0..<(selectedCountry?.languages.count)! {
+            let idiom = selectedCountry!.languages[language]
+            languagesLabel.text! += "\(idiom.name). "
+        }
     }
     
     override func viewDidDisappear(_ animated: Bool) {
--- Challenge(project13-15)/Challenge(project13-15)/ViewController.swift
+++ Challenge(project13-15)/Challenge(project13-15)/ViewController.swift
@@ -8,19 +8,21 @@
 import UIKit
 
 class ViewController: UITableViewController {
-
+    
     var contries = [Country]()
+    var nonFilterArray = [Country]()
+    var filterArray = [Country]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .search, target: self, action: nil)
-        
+        navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .search, target: self, action: #selector(inputFilter))
+        title = "REST Countries API"
         loadJsonFromWeb()
-
     }
     
     //MARK: - load and decode data from web
+    
     func loadJsonFromWeb() {
         let urlString = "https://restcountries.eu/rest/v2/all?fields=name;capital;currencies;subregion;population;languages;flag"
         if let url = URL(string: urlString) {
@@ -36,28 +38,65 @@ class ViewController: UITableViewController {
         let decoder = JSONDecoder()
         
         if let jsonContries: [Country] = try? decoder.decode([Country].self, from: json) {
-            contries.append(contentsOf: jsonContries)
+            nonFilterArray.append(contentsOf: jsonContries)
+            contries = nonFilterArray
             tableView.reloadData()
         }
     }
     
-//    MARK: - tableView Functions
+    //MARK: - Filter functions
+    
+    @objc func inputFilter() {
+        let ac = UIAlertController(title: "Filter", message: nil, preferredStyle: .alert)
+        ac.addTextField { (textField) in
+            textField.placeholder = "Type the country name"
+        }
+        
+        let submitAction = UIAlertAction(title: "Filter", style: .default) { [weak self, weak ac] _ in
+            guard let answer = ac?.textFields?[0].text else { return }
+            self?.filterCountries(answer)
+        }
+        
+        ac.addAction(submitAction)
+        present(ac, animated: true)
+    }
+    
+    func filterCountries(_ answer: String) {
+        filterArray.removeAll()
+        
+        for country in contries {
+            if country.name.lowercased().contains(answer.lowercased()) {
+                filterArray.insert(country, at: 0)
+            }
+        }
+        contries = filterArray
+        navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .stop, target: self, action: #selector(unFilter))
+        tableView.reloadData()
+    }
+    
+    @objc func unFilter() {
+        contries = nonFilterArray
+        navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .search, target: self, action: #selector(inputFilter))
+        tableView.reloadData()
+    }
+    
+    //    MARK: - tableView Functions
+    
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         if let vc = storyboard?.instantiateViewController(identifier: "DetailViewController") as? DetailViewController {
             vc.selectedCountry = contries[indexPath.row]
-            print(contries[indexPath.row].flag)
             navigationController?.pushViewController(vc, animated: true)
         }
     }
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "CountriesList", for: indexPath)
         let country = contries[indexPath.row]
         cell.textLabel?.text = country.name
-//        cell.detailTextLabel?.text = country.capital
         
         return cell
-        
     }
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return contries.count
     }
